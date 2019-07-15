807d0882d8b41bc93a7e7888e73a2943afa62cf8
zhaoyuanguang <zhaoyuanguang1573.@163.com>
2019-07-15 23:11:45 +0800

    dubbo构建ssm分布式系统

---
+++ distributed_ssm_dubbo_parent/goods_interface/src/main/java/com/zyg/goods/service/GoodsService.java
@@ -0,0 +1,56 @@
+package com.zyg.goods.service;
+import java.util.List;
+import com.zyg.pojo.TbGoods;
+
+import entity.PageResult;
+/**
+ * 服务层接口
+ * @author Administrator
+ *
+ */
+public interface GoodsService {
+
+	/**
+	 * 返回全部列表
+	 * @return
+	 */
+	public List<TbGoods> findAll();
+	
+	
+	/**
+	 * 返回分页列表
+	 * @return
+	 */
+	public PageResult findPage(int pageNum, int pageSize);
+
+	
+	/**
+	 * 修改
+	 */
+	public void update(TbGoods goods);
+	
+
+	/**
+	 * 根据ID获取实体
+	 * @param id
+	 * @return
+	 */
+	public TbGoods findOne(Long id);
+	
+	
+	/**
+	 * 批量删除
+	 * @param ids
+	 */
+	public void delete(Long[] ids);
+
+	/**
+	 * 条件分页查询
+	 * @param goods
+	 * @param pageNum
+	 * @param pageSize
+	 * @return
+	 */
+	public PageResult findPage(TbGoods goods, int pageNum, int pageSize);
+	
+}
